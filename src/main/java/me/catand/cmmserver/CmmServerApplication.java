d2e14855dad3b1b5fbbc5451ff793422bcebcae4
catandA <75292299+catandA@users.noreply.github.com>
2024-01-14 13:27:42 +0800

    初始化

---
+++ src/main/java/me/catand/cmmserver/CmmServerApplication.java
@@ -0,0 +1,13 @@
+package me.catand.cmmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmmServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/catand/cmmserver/CmmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.catand.cmmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
